ae18b51fcca21d2d66070e80fe084f396bf12cff
ayushpasi <pasiayush999@gmail.com>
2024-07-31 14:00:13 +0530

    adding people in db

--- myApp.js
+++ myApp.js
@@ -9,14 +9,32 @@ const personSchema = new Schema({
   favoriteFoods: [String],
 });
 Person = mongoose.model("Person", personSchema);
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+var createAndSavePerson = function (done) {
+  var janeFonda = new Person({
+    name: "Jane Fonda",
+    age: 84,
+    favoriteFoods: ["eggs", "fish", "fresh fruit"],
+  });
+
+  janeFonda.save(function (err, data) {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
-};
+/** 4) Create many People with `Model.create()` */
+var arrayOfPeople = [
+  { name: "Frankie", age: 74, favoriteFoods: ["Del Taco"] },
+  { name: "Sol", age: 76, favoriteFoods: ["roast chicken"] },
+  { name: "Robert", age: 78, favoriteFoods: ["wine"] },
+];
 
+var createManyPeople = function (arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
+};
 const findPeopleByName = (personName, done) => {
   done(null /*, data*/);
 };
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const enableCORS = function (req, res, next) {
 
 // global setting for safety timeouts to handle possible
 // wrong callbacks that will never be called
-const TIMEOUT = 10000;
+const TIMEOUT = 1000;
 
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(bodyParser.json());
