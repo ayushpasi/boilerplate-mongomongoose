2c1bb3aac150a65b1ac7765d339f1af315b9cdbc
ayushpasi <pasiayush999@gmail.com>
2024-07-31 23:38:47 +0530

    findEditThenSave

--- myApp.js
+++ myApp.js
@@ -59,8 +59,14 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById({ _id: personId }, (error, person) => {
+    if (error) return console.log(error);
+    person.favoriteFoods.push(foodToAdd);
+    person.save((error, updatedPerson) => {
+      if (error) return console.log(error);
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
